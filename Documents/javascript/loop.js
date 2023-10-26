b36956334ae49e836483a3671a55c16883eab985
Sesan <sesanmania@gmail.com>
2023-10-26 05:49:51 +0100

    loop

---
+++ Documents/javascript/loop.js
@@ -0,0 +1,11 @@
+// let number =[1, 2, 3, 4, 5]
+// let double = number.map((number) => number *2)
+// console.log(double);
+
+//for loops
+
+let number = [1, 2, 3, 4, 5]
+for (let i = 0; i < number.length; i++){
+    number [i] = number [i] * 2
+}
+console.log(number);
