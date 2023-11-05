38546b748b9c522653c3f9422716688e33082ce7
Sesan <sesanmania@gmail.com>
2023-11-05 17:05:05 +0100

    atm task

---
+++ Documents/atm task/atm.js
@@ -0,0 +1,24 @@
+function minimalBills (amount){
+let money100 = 0;
+let money20 = 0;
+let money9 = 0;
+let money1 = 0;
+money100 = Math.floor (amount / 100);
+amount %= 100;
+money20 = Math.floor (amount / 20);
+amount %= 20;
+money9 = Math.floor (amount / 9);
+amount %=9;
+money1 = amount;
+return [money100, money20, money9, money1];
+}
+const amount = 1051;
+const [money100, money20, money9,money1] = minimalBills(amount);
+console.log(`minimal number of 100 naira bilss: ${money100}`);
+console.log(` minimal number of 20 naira bills: ${money20}`);
+console.log(` minimal number of 9 naira bills: ${money9}`);
+console.log(` minimal number of 1 naira bills: ${money1}`);
+
+
+
+
