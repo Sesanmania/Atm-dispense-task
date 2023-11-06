c7b4af6fed2fc41c5f8ff58308ea39d33e2b2ea9
Sesan <sesanmania@gmail.com>
2023-11-06 11:34:45 +0100

    amt ass

--- Documents/atm task/atm.js
+++ Documents/atm task/atm.js
@@ -12,13 +12,22 @@ amount %=9;
 money1 = amount;
 return [money100, money20, money9, money1];
 }
-const amount = 1051;
+const amount = 2034;
 const [money100, money20, money9,money1] = minimalBills(amount);
 console.log(`minimal number of 100 naira bilss: ${money100}`);
 console.log(` minimal number of 20 naira bills: ${money20}`);
 console.log(` minimal number of 9 naira bills: ${money9}`);
 console.log(` minimal number of 1 naira bills: ${money1}`);
 
+
+function theSchool (str){
+    return str.replace (-_);
+    function str (group){
+        return group.toupperCase();
+
+    }
+}
+console.log(theSchool);
 
 
 
