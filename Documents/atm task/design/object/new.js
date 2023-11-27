f5c838e1cde3d0eea4039293223d60d19e5d456b
Sesan <sesanmania@gmail.com>
2023-11-27 11:40:51 +0100

    object practise

--- Documents/atm task/design/object/new.js
+++ Documents/atm task/design/object/new.js
@@ -6,5 +6,18 @@ const studentName = {
     course: 'geophysic',
 
 }
- console.log(studentName['otherName']);
+//  console.log(studentName['otherName']);
 
+ const myFavouriteBooks = {
+  tittle: 'lord of the ring',
+  topic: 'apple of my eyes',
+ }
+// console.log(myFavouriteBooks['topic']);
+
+const myFavouriteCar ={
+    1: 'toyota',
+    2: 'honda',
+    3: 'camry',
+    'car type': 'benz',
+}
+console.log(myFavouriteCar['car type']);
